53e10219133ee7a3bf383d839f4feb6d58b9c9c8
gustavofabro <gustavofabro.f@gmail.com>
2020-09-21 20:24:45 -0300

    use test deps

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "./test_deps.ts";
 import { clone } from "./mod.ts";
 
 Deno.test("it should be able to clone surface properties", () => {
