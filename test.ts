4e27c23f44044c9fe1d25b4cfaae40e7ba2d134f
gustavofabro <gustavofabro.f@gmail.com>
2020-09-22 20:13:03 -0300

    add test for function clone

--- test.ts
+++ test.ts
@@ -4,25 +4,31 @@ import { clone } from "./mod.ts";
 Deno.test("it should be able to clone surface properties", () => {
   const objA = {
     foo: "foo",
+    barFn: () => "bar",
   };
 
   const objB = clone(objA);
 
   objB.foo = "bar";
+  objB.barFn = () => "foo";
 
   assertEquals(objA.foo, "foo");
+  assertEquals(objA.barFn(), "bar");
 });
 
 Deno.test("it should be able to clone deep properties", () => {
   const objA = {
     foo: {
       bar: "bar",
+      barFn: () => "bar",
     },
   };
 
   const objB = clone(objA);
 
   objB.foo.bar = "foo";
+  objB.foo.barFn = () => "foo";
 
   assertEquals(objA.foo.bar, "bar");
+  assertEquals(objA.foo.barFn(), "bar");
 });
